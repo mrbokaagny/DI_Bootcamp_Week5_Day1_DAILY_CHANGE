f47ba0f352c5d312322d237edf88fa2b54d89f9a
mrdevbanks <103212600+mrdevbanks@users.noreply.github.com>
2023-01-19 04:21:20 +0000

    projecct fini

---
+++ start-api/src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Films } from "../models/films.model";
+import { resolve } from "dns";
+
+
+@Injectable()
+
+export class ApiService {
+
+  constructor(private client: HttpClient) { }
+
+  films: Films[] = []
+  endPoint: string = 'https://swapi.dev/api/films'
+
+  callApi(): Promise<Films[]> {
+    return new Promise((resolve, reject) => {
+      this.client.get(this.endPoint).subscribe((data: any) => {
+        let result: Films[] = data.results.map((item: any) =>
+          new Films(item.title, item.episode_id, item.opening_crawl, item.director , item.producer , item.release_date)
+        )
+        resolve(result)
+      },
+        (err) => {
+          reject(err)
+        }
+      )
+    })
+  }
+
+}
